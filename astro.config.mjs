205814e52b2516820e1576d415cab0a6390710d3
Aruh1 <rizkypraschool@gmail.com>
2026-07-05 01:55:58 +0700

    feat: use custom domain cr.yuramedia.com, add sitemap integration, config withastro/action and public CNAME

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import tailwind from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://cr.yuramedia.com',
   base: '/',
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwind()],
   },
